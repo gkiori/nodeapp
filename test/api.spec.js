c70c90d692bae2e63cd5ff4fa101157909857d5a
gkiori <gkastane@gmail.com>
2022-08-19 10:26:57 +0200

    modif fichier test v2

--- test/api.spec.js
+++ test/api.spec.js
@@ -1,11 +1,12 @@
-import {chai} from "chai";
-var expect = chai.expect;
+import {expect} from "chai";
 
 describe('Simple Math Test', () => {
     it('should return 2', function () {
-        assert.equal(1+1, 2);
+        //assert.equal(1+1, 2);
+        expect(1+1).to.equal(2);
     });
     it('should return 9', function () {
-        assert.equal(3*3, 9)
+        //assert.equal(3*3, 9);
+        expect(3*3).to.equal(9);
     });
 });
